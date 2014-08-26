a5f2d02ba5fd0098ce08a655d63db3e033e96a1a
Muhammed Siju <msiju@codeaurora.org>
2014-08-26 20:19:41 +0530

    Fix back to back data call requests on same apn.
    If one ApnContext is in DISCONNECTING state and another ApnContext
    which uses the same apn needs to be brought up, current code doesnot
    share the same DataConnection object. This may lead to back to back
    requests on same apn. To avoid this, allow sharing of DC even when it
    is in DISCONNECTING state. This will allow DC to be brought up immediately
    after getting disconnected.
    
    CRs-Fixed: 712333
    Change-Id: I3d6fef87014f339b04393ec33f86d89beab8c644

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -1698,6 +1698,14 @@ public final class DcTracker extends DcTrackerBase {
                             case CONNECTING:
                                 potentialDcac = curDcac;
                                 potentialApnCtx = curApnCtx;
+                                break;
+                            case DISCONNECTING:
+                                //Update for DISCONNECTING only if there is no other potential match
+                                if (potentialDcac == null) {
+                                    potentialDcac = curDcac;
+                                    potentialApnCtx = curApnCtx;
+                                }
+                                break;
                             default:
                                 // Not connected, potential unchanged
                                 break;
@@ -1716,6 +1724,16 @@ public final class DcTracker extends DcTrackerBase {
                         case CONNECTING:
                             potentialDcac = curDcac;
                             potentialApnCtx = curApnCtx;
+                            break;
+                        case DISCONNECTING:
+                            // Update for DISCONNECTING only if there is no other potential match
+                            // and the apns are same
+                            if (potentialDcac == null &&
+                                    apnSetting.equals(apnContext.getNextWaitingApn())) {
+                                potentialDcac = curDcac;
+                                potentialApnCtx = curApnCtx;
+                            }
+                            break;
                         default:
                             // Not connected, potential unchanged
                             break;
