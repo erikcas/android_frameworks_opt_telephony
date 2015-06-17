241adb91f850e41dda4e3bd0b16d2760d4ede18d
Sandeep Kunta <skunta@codeaurora.org>
2015-06-17 17:58:36 +0530

    Initialize call state phone app starts
    If phone process crashed during an active voice call, even after restart
    of phone app, clients still get call state as OFFHOOK since
    TelephonyRegistry has the old infomration and whenever new client is
    registered that old infomration is given resulting in erroneous UI
    behavior. Present fix sends call state update during when phone app
    starts so that TelephonyRegistry resets its information.
    
    Change-Id: I709a8b4d78c50099c021ad5f78bbc9fdf6db6585
    CRs-Fixed: 906539

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -171,6 +171,7 @@ public class GSMPhone extends PhoneBase {
         mSST.registerForNetworkAttached(this, EVENT_REGISTERED_TO_NETWORK, null);
         mCi.setOnSs(this, EVENT_SS, null);
         setProperties();
+        notifyPhoneStateChanged();
     }
 
     public
@@ -210,6 +211,7 @@ public class GSMPhone extends PhoneBase {
         log("GSMPhone: constructor: sub = " + mPhoneId);
 
         setProperties();
+        notifyPhoneStateChanged();
     }
 
     protected void setProperties() {
