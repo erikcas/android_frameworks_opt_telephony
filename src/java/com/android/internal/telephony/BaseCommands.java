6137658a90b08be3c3b326a8e7e3a44a5560c49b
Wileen Chiu <wileenc@codeaurora.org>
2015-08-12 17:31:13 -0700

    Add support for send Message with messaging options.
    Add support for following messaging options.
    
    1.Add support for message priority in 3gpp2.
    
    2.Add link control support while sending multiple messages.
    
    3.Add validity period support in 3gpp.
    
    Change-Id: Iaee8b93ef373e34ddcc5b7d65365852e470467d4
    
    Pass correct arguments to sendText api
    
    Pass correct arguments to sendText api
    
    Change-Id: I3b07c7c359af12fe6d63636408d4d6237acff8cd
    
    MSIM(Telephony):Add changes to SMS APIs as per new SmsManager design
    
    Changed SMS Msim APIs as per new SmsManager design.
    
    Added SMS Msim APIs for new SmsManager
    relevant to adding support for sending Message
    with messaging options.
    
    Change-Id: I53bb027531959f71285e5f79896768a591129536

--- src/java/com/android/internal/telephony/BaseCommands.java
+++ src/java/com/android/internal/telephony/BaseCommands.java
@@ -812,6 +812,9 @@ public abstract class BaseCommands implements CommandsInterface {
         }
     }
 
+    public void sendSMSExpectMore (String smscPDU, String pdu, Message result) {
+    }
+
     protected void onRadioAvailable() {
     }
 
