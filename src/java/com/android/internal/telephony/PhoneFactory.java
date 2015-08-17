412fdd550b57bfbb9177edab93f0dd46d3b60da9
Sandeep Gutta <sangutta@codeaurora.org>
2015-08-17 18:29:52 +0530

    MSIM: N/W mode based flex map related changes
    - Add GSMPhone/CDMALTEPhone create method in TelephonyPlugin
      to have  provision of extending it from proprietary space.
    - Introduce new method initExtTelephonyClasses() in TelephonyPlugin
      to instantiate new classes from proprietary space.
    
    Change-Id: I2161f9d5a2150298fa283245929f2daca5f8b8e8

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -149,10 +149,10 @@ public class PhoneFactory {
                     PhoneBase phone = null;
                     int phoneType = TelephonyManager.getPhoneType(networkModes[i]);
                     if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
-                        phone = new GSMPhone(context,
+                        phone = TelephonyPluginDelegate.getInstance().makeGSMPhone(context,
                                 sCommandsInterfaces[i], sPhoneNotifier, i);
                     } else if (phoneType == PhoneConstants.PHONE_TYPE_CDMA) {
-                        phone = new CDMALTEPhone(context,
+                        phone = TelephonyPluginDelegate.getInstance().makeCDMALTEPhone(context,
                                 sCommandsInterfaces[i], sPhoneNotifier, i);
                     }
                     Rlog.i(LOG_TAG, "Creating Phone with type = " + phoneType + " sub = " + i);
@@ -187,6 +187,9 @@ public class PhoneFactory {
                 sSubInfoRecordUpdater = TelephonyPluginDelegate.getInstance().
                         makeSubscriptionInfoUpdater(context, sProxyPhones, sCommandsInterfaces);
                 SubscriptionController.getInstance().updatePhonesAvailability(sProxyPhones);
+
+                TelephonyPluginDelegate.getInstance().
+                        initExtTelephonyClasses(context, sProxyPhones, sCommandsInterfaces);
             }
         }
     }
@@ -194,16 +197,16 @@ public class PhoneFactory {
     public static Phone getCdmaPhone(int phoneId) {
         Phone phone;
         synchronized(PhoneProxy.lockForRadioTechnologyChange) {
-            phone = new CDMALTEPhone(sContext, sCommandsInterfaces[phoneId],
-                    sPhoneNotifier, phoneId);
+            phone = TelephonyPluginDelegate.getInstance().makeCDMALTEPhone(sContext,
+                    sCommandsInterfaces[phoneId], sPhoneNotifier, phoneId);
         }
         return phone;
     }
 
     public static Phone getGsmPhone(int phoneId) {
         synchronized(PhoneProxy.lockForRadioTechnologyChange) {
-            Phone phone = new GSMPhone(sContext, sCommandsInterfaces[phoneId],
-                    sPhoneNotifier, phoneId);
+            Phone phone = TelephonyPluginDelegate.getInstance().makeGSMPhone(sContext,
+                    sCommandsInterfaces[phoneId], sPhoneNotifier, phoneId);
             return phone;
         }
     }
