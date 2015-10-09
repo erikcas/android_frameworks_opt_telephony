5db24acf7fd9bc804dea25103c93ec8cc0881aa6
Nivedita Sarkar <nsarkar@codeaurora.org>
2015-10-09 16:19:35 -0700

    IMS: Notify supplementary service failed for call hold failures
    - We need to notify supplementary service failed in case call
      hold fails.
    
    Change-Id: Id041663206a8c71dec91ecc1cbda403ca14cc88d
    CRs-Fixed: 919007

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1267,6 +1267,7 @@ public final class ImsPhoneCallTracker extends CallTracker {
                         sendEmptyMessageDelayed(EVENT_HANGUP_PENDINGMO, TIMEOUT_HANGUP_PENDINGMO);
                     }
                 }
+                mPhone.notifySuppServiceFailed(Phone.SuppService.SWITCH);
             }
         }
 
